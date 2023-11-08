66ba1d8a95e0691e44005f33c30f8fd24856b2c2
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2023-11-07 20:11:11 -0500

    0.0.1

--- be-exporting.js
+++ be-exporting.js
@@ -29,42 +29,46 @@ export class BeExporting extends BE {
             const attr = enhancedElement.getAttribute(eventName);
             if (attr === null)
                 throw 404;
-            let scriptEl;
-            if (!scriptLookup.has(attr)) {
-                scriptEl = document.createElement('script');
-                scriptEl.type = 'module';
-                scriptEl._exports = {};
-                scriptLookup.set(attr, scriptEl);
-                const key = crypto.randomUUID();
-                window[key] = scriptEl;
-                const splitText = attr.split('export const ');
-                const winKey = `window['${key}']`;
-                for (let i = 1, ii = splitText.length; i < ii; i++) {
-                    const token = splitText[i];
-                    const iPosOfEq = token.indexOf('=');
-                    const lhs = token.substr(0, iPosOfEq).trim();
-                    splitText[i] = `const ${lhs}  = ${winKey}._exports.${lhs} = ${token.substr(iPosOfEq + 1)};`;
-                }
-                let modifiedText = splitText.join('');
-                modifiedText = /* js */ `
-                ${modifiedText}
-                window['${key}'].dispatchEvent(new Event('load'));
-                window['${key}'].dataset.loaded = 'true';
-                `;
-                scriptEl.innerHTML = modifiedText;
-                document.head.appendChild(scriptEl);
-            }
-            else {
-                scriptEl = scriptLookup.get(attr);
-            }
-            await waitForScriptToLoad(scriptEl);
-            self[eventName] = { ...scriptEl._exports };
+            const exports = await tbd(attr);
+            self[eventName] = { ...exports };
             console.log({ attr });
         }
         return {
             resolved: true
         };
     }
+}
+export async function tbd(attr) {
+    let scriptEl;
+    if (!scriptLookup.has(attr)) {
+        scriptEl = document.createElement('script');
+        scriptEl.type = 'module';
+        scriptEl._exports = {};
+        scriptLookup.set(attr, scriptEl);
+        const key = crypto.randomUUID();
+        window[key] = scriptEl;
+        const splitText = attr.split('export const ');
+        const winKey = `window['${key}']`;
+        for (let i = 1, ii = splitText.length; i < ii; i++) {
+            const token = splitText[i];
+            const iPosOfEq = token.indexOf('=');
+            const lhs = token.substr(0, iPosOfEq).trim();
+            splitText[i] = `const ${lhs}  = ${winKey}._exports.${lhs} = ${token.substr(iPosOfEq + 1)};`;
+        }
+        let modifiedText = splitText.join('');
+        modifiedText = /* js */ `
+        ${modifiedText}
+        window['${key}'].dispatchEvent(new Event('load'));
+        window['${key}'].dataset.loaded = 'true';
+        `;
+        scriptEl.innerHTML = modifiedText;
+        document.head.appendChild(scriptEl);
+    }
+    else {
+        scriptEl = scriptLookup.get(attr);
+    }
+    await waitForScriptToLoad(scriptEl);
+    return scriptEl._exports;
 }
 export function waitForScriptToLoad(scriptEl) {
     return new Promise((resolve) => {
